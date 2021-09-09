62e9e21647746be395eb398b2314f067d1346fd2
Slava <vsvoiteshonok1@gmail.com>
2021-09-09 12:41:29 +0300

    1 lab done

---
+++ WEB_VOITESHONOK_953501/WEB_VOITESHONOK_953501/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WEB_VOITESHONOK_953501.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
