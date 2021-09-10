7d9de678c1ca4fb5fd35dc3d45a8e473e7d3ce5a
Slava <vsvoiteshonok1@gmail.com>
2021-09-11 01:54:44 +0300

    lab 2 done

--- WEB_VOITESHONOK_953501/WEB_VOITESHONOK_953501/Controllers/HomeController.cs
+++ WEB_VOITESHONOK_953501/WEB_VOITESHONOK_953501/Controllers/HomeController.cs
@@ -1,37 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using WEB_VOITESHONOK_953501.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WEB_VOITESHONOK_953501.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private List<ListDemo> _listDemo;
 
         public IActionResult Index()
         {
+            ViewData["Text"] = "Лабораторная работа 2";
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
+
             return View();
         }
 
-        public IActionResult Privacy()
+        public HomeController()
         {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            _listDemo = new List<ListDemo>
+            {
+                new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+                new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+                new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+            };
         }
     }
+
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+    }
+
 }
